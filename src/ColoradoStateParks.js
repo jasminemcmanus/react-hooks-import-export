7bfeee4e54b0e8eee0ce4f1b60031dfb23990985
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-07-19 21:28:57 +1000

    done with assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+import { trees, wildlife } from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
-}
+  return (
+    <React.Fragment>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </React.Fragment>
+)
+} 
+
+console.log(RMFunctions.trees);
+RMFunctions.wildlife();
+console.log(trees);
+wildlife();
+
+export default ColoradoStateParks;
